da57a4bd9d845299685647ef6c3339d71bf3cbe8
Cooper Brislain <cbrislain@gmail.com>
2020-09-21 13:26:36 -0700

    added basic wifi & mqtt

--- AirQualityMonitor.h
+++ AirQualityMonitor.h
@@ -2,12 +2,17 @@
 #define AIRQUALITYMONITOR_H
 
 #include <Arduino.h>
-
 #include <driver/adc.h>
 
+#include <WiFi.h>
+#include <PubSubClient.h>
 #include <string>
 #include <iostream>
 
+#include "config.h"
+
 //#include "StreamPrint.h"
+
+void setup_wifi();
 
 #endif // AIRQUALITYMONITOR_H
